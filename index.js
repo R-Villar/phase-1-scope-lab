c951cdc9f8e94ee97d084494593493fbd6062190
Remberto Villar <rembertovillar@gmail.com>
2022-06-30 12:13:51 -0400

    commit tree c7f6b5eccf44422ab826fff4bc581924fe884a1f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const LeastFavoriteCustomer = 'Rem'
+
+function upperCaseCustomerName() {
+    customerName =  customerName.toLocaleUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+   
+    LeastFavoriteCustomer = 'not rem'
+}
